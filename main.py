d4a1c45f86f765e471b20c8791b85ba564d843f1
koko <github@koko-campus.com>
2022-09-15 12:32:05 +0900

    コマンドライン引数用パラメタの修正。

--- main.py
+++ main.py
@@ -7,34 +7,34 @@ if __name__ != "__main__":
 	sys.exit(1)
 
 COMMAND = "./target/release/mandelbrot_rs"
-COUNT = 100
 ASPECT_RATIO=1.5
-SHRINK_RATIO = 0.95
+SHRINK_RATIO = 0.99
 FILESIZE_HEIGHT=10000
 START_X = -1.2
 START_Y = 0.35
 
 DEFAULT_WIDTH = 0.2
-DEFAULT_HEIGHT = -0.15
+DEFAULT_HEIGHT = 0.15
 
 
 def processor(file_name, size, upper_left, lower_right):
-	print("py -> ", [COMMAND, file_name + ".png", size, upper_left, lower_right])
-	subprocess.call([COMMAND, file_name + ".png", size, upper_left, lower_right])
+	#print("py -> ", [COMMAND, file_name + ".png", size, upper_left, lower_right])
+	#subprocess.Popen([COMMAND, file_name + ".png", size, upper_left, lower_right]) # 非同期実行
+	subprocess.run([COMMAND, file_name + ".png", size, upper_left, lower_right]) # 同期実行
 	return
 
 height = DEFAULT_HEIGHT
 
-for i in range(0, 3):
-	delta_x = height * ASPECT_RATIO
-	delta_y = height
-	new_start_x = delta_x / 2 + START_X
-	new_start_y = delta_y / 2 + START_Y
+for i in range(10, 10000):
+	height =  DEFAULT_HEIGHT * SHRINK_RATIO ** i
+	cSize_x = height * ASPECT_RATIO # 横幅
+	cSize_y = height # 縦幅
+	new_start_x = START_X + ((DEFAULT_WIDTH - cSize_x) / 2) # 左上のx座標
+	new_start_y = START_Y - ((DEFAULT_HEIGHT - cSize_y) / 2) # 左上のy座標
 	file_name = "./target/{}".format(i)
 	size = "{0}x{1}".format(math.floor(FILESIZE_HEIGHT * ASPECT_RATIO), math.floor(FILESIZE_HEIGHT))
 	upper_left = "{0},{1}".format(new_start_x, new_start_y)
-	lower_right = "{0},{1}".format(new_start_x + height * ASPECT_RATIO, new_start_y - height)
+	lower_right = "{0},{1}".format(new_start_x + cSize_x, new_start_y - cSize_y)
 	processor(file_name, size, upper_left, lower_right)
-	height *= SHRINK_RATIO
 
 # /home/mandelbrot.rs/target/release/mandelbrot_rs image.png 40000x30000 -1.20,0.35 -1.0,0.20 &
