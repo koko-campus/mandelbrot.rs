fa99dd099467710e0924c0d2d33e79c0f5347c44
koko <github@koko-campus.com>
2022-09-15 13:00:43 +0900

    デフォルト値の記述を追加。

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ if __name__ != "__main__":
 	sys.exit(1)
 
 COMMAND = "./target/release/mandelbrot_rs"
-ASPECT_RATIO=1.5
-SHRINK_RATIO = 0.99
-FILESIZE_HEIGHT=10000
-START_X = -1.2
-START_Y = 0.35
+ASPECT_RATIO=1.5 # DEFAULT [1.5]
+SHRINK_RATIO = 0.99 # DEFAULT [0.99]
+FILESIZE_HEIGHT=1000 # DEFAULT [10000]
+START_X = -1.5 # DEFAULT [-1.2]
+START_Y = 0.5 # DEFAULT [0.35]
 
-DEFAULT_WIDTH = 0.2
-DEFAULT_HEIGHT = 0.15
+DEFAULT_WIDTH = 0.35 # DEFAULT [0.2]
+DEFAULT_HEIGHT = 0.2 # DEFAULT [0.15]
 
 
 def processor(file_name, size, upper_left, lower_right):
@@ -25,13 +25,13 @@ def processor(file_name, size, upper_left, lower_right):
 
 height = DEFAULT_HEIGHT
 
-for i in range(10, 10000):
+for i in range(100, 300):
 	height =  DEFAULT_HEIGHT * SHRINK_RATIO ** i
 	cSize_x = height * ASPECT_RATIO # 横幅
 	cSize_y = height # 縦幅
 	new_start_x = START_X + ((DEFAULT_WIDTH - cSize_x) / 2) # 左上のx座標
 	new_start_y = START_Y - ((DEFAULT_HEIGHT - cSize_y) / 2) # 左上のy座標
-	file_name = "./fruits/{}".format(i)
+	file_name = "./debug/{}".format(i)
 	size = "{0}x{1}".format(math.floor(FILESIZE_HEIGHT * ASPECT_RATIO), math.floor(FILESIZE_HEIGHT))
 	upper_left = "{0},{1}".format(new_start_x, new_start_y)
 	lower_right = "{0},{1}".format(new_start_x + cSize_x, new_start_y - cSize_y)
