bf2acdc604fd0bf2bde924bc7f535a573fcdad0d
koko <github@koko-campus.com>
2022-09-15 20:19:57 +0900

    新しい成果物用にプログラムを書き換え。

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ COMMAND = "./target/release/mandelbrot_rs"
 ASPECT_RATIO=1.5 # DEFAULT [1.5]
 SHRINK_RATIO = 0.99 # DEFAULT [0.99]
 FILESIZE_HEIGHT=1000 # DEFAULT [10000]
-START_X = -1.21990088 # DEFAULT [-1.2]
-START_Y = 0.335100082 # DEFAULT [0.35]
+START_X = -2.3 # DEFAULT [-1.2]
+START_Y = 1.02 # DEFAULT [0.35]
 
-DEFAULT_WIDTH = 0.2 # DEFAULT [0.2]
-DEFAULT_HEIGHT = 0.15 # DEFAULT [0.15]
+DEFAULT_WIDTH = 3.1000299996765301 # DEFAULT [0.2]
+DEFAULT_HEIGHT = 2.0000816899999 # DEFAULT [0.15]
 
 
 def processor(file_name, size, upper_left, lower_right):
@@ -27,15 +27,15 @@ def processor(file_name, size, upper_left, lower_right):
 height = DEFAULT_HEIGHT
 
 name = input("enter name...").strip()
-os.makedirs("./seeds/{}".format(name)) 
+os.makedirs("./seeds/{}".format(name), exist_ok=True) 
 
-for i in range(0, 1800):
+for i in range(3600, 3601):
 	height =  DEFAULT_HEIGHT * SHRINK_RATIO ** i
 	cSize_x = height * ASPECT_RATIO # 横幅
 	cSize_y = height # 縦幅
 	new_start_x = START_X + ((DEFAULT_WIDTH - cSize_x) / 2) # 左上のx座標
 	new_start_y = START_Y - ((DEFAULT_HEIGHT - cSize_y) / 2) # 左上のy座標
-	file_name = "./seeds/{}/{}".format(name, str(i).zfill(8))
+	file_name = "./seeds/{}/{}".format(name, str("00000000").zfill(8))
 	size = "{0}x{1}".format(math.floor(FILESIZE_HEIGHT * ASPECT_RATIO), math.floor(FILESIZE_HEIGHT))
 	upper_left = "{0},{1}".format(new_start_x, new_start_y)
 	lower_right = "{0},{1}".format(new_start_x + cSize_x, new_start_y - cSize_y)
