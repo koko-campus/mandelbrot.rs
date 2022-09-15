c2e2fb0b80326c010b5228602d3747b5422a977d
koko <github@koko-campus.com>
2022-09-15 11:37:45 +0900

    pythonスクリプトの作成。

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import subprocess
 import sys
-print(__name__)
+import math
+
 if __name__ != "__main__":
 	print("call me directly!!")
 	sys.exit(1)
 
-COMMAND = "./release/mandelbrot_rs {0}.png {1} {2} {3}"
+COMMAND = "./target/release/mandelbrot_rs"
 COUNT = 100
 ASPECT_RATIO=1.5
 SHRINK_RATIO = 0.95
@@ -13,27 +14,27 @@ FILESIZE_HEIGHT=10000
 START_X = -1.2
 START_Y = 0.35
 
-DEFAULT_HEIGHT = -0.15
 DEFAULT_WIDTH = 0.2
+DEFAULT_HEIGHT = -0.15
 
 
-def processor(command):
-	print(command)
-	#subprocess.call(command)
+def processor(file_name, size, upper_left, lower_right):
+	print("py -> ", [COMMAND, file_name + ".png", size, upper_left, lower_right])
+	subprocess.call([COMMAND, file_name + ".png", size, upper_left, lower_right])
 	return
 
-width = DEFAULT_WIDTH
+height = DEFAULT_HEIGHT
 
-for i in range(0, COUNT):
-	delta_x = DEFAULT_WIDTH - width
-	delta_y = DEFAULT_HEIGHT - width / ASPECT_RATIO
+for i in range(0, 3):
+	delta_x = height * ASPECT_RATIO
+	delta_y = height
 	new_start_x = delta_x / 2 + START_X
 	new_start_y = delta_y / 2 + START_Y
-	file_name = "target/{}".format(i)
-	size = "{0}x{1}".format(FILESIZE_WIDTH, FILESIZE_WIDTH / ASPECT_RATIO)
+	file_name = "./target/{}".format(i)
+	size = "{0}x{1}".format(math.floor(FILESIZE_HEIGHT * ASPECT_RATIO), math.floor(FILESIZE_HEIGHT))
 	upper_left = "{0},{1}".format(new_start_x, new_start_y)
-	lower_right = "{0},{1}".format(new_start_x + width, new_start_y - width / ASPECT_RATIO)
-	processor(COMMAND.format(file_name, size, upper_left, lower_right))
-	width *= SHRINK_RATIO
+	lower_right = "{0},{1}".format(new_start_x + height * ASPECT_RATIO, new_start_y - height)
+	processor(file_name, size, upper_left, lower_right)
+	height *= SHRINK_RATIO
 
-# /home/mandelbrot.rs/target/release/mandelbrot_rs super_big.png 4000000x3000000 -1.20,0.35 -1.0,0.20 &
+# /home/mandelbrot.rs/target/release/mandelbrot_rs image.png 40000x30000 -1.20,0.35 -1.0,0.20 &
